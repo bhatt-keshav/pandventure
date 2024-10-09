6cccaebaad6b782a626aba3623d5f96f22b6a5f7
bhatt-keshav <53868299+bhatt-keshav@users.noreply.github.com>
2024-10-09 20:26:17 +0200

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,2 +1,5 @@
 # Creates a series of dates with a frequency of 1 month
 pd.date_range(start='1/1/2024', periods=12, freq='M')
+
+# Creates a daily series. Note the format can either be with / or -
+date_series = pd.date_range(start='2022-12-01', end=today, freq='D')
